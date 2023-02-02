3d5ca659df7838c47b1b211dd91039105975014a
GustavoLimaSP <123516644+GustavoLimaSP@users.noreply.github.com>
2023-02-01 21:42:05 -0300

    Criação da classe Pneu

---
+++ Montadora/ConsoleAppPrincipal/Program.cs
@@ -0,0 +1,27 @@
+﻿using Models;
+using System.Runtime.ConstrainedExecution;
+
+namespace ConsoleAppPrincipal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pneu pneu1 = new Pneu(16, 150, "carro de passeio: ", false);
+
+            Pneu pneu2 = new Pneu(16, 70, "Pneu de estepe: ", true);
+           
+            pneu1.Girar(6);
+            pneu2.Girar(6);
+            pneu2.Girar(15);
+            pneu2.Girar(10);
+            pneu2.Frear(5);
+            pneu2.Girar(20);
+            pneu2.Girar(20);
+            pneu2.Girar(5);
+            pneu1.Exibir();
+            Console.WriteLine();
+            pneu2.Exibir();
+        }
+    }
+}
---
+++ Montadora/Models/Pneu.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Models
+{
+    public class Pneu
+    {
+        public string Cor { get; set; }
+        public int Aro { get; set; }
+        public string tipo { get; set; }
+        public bool TWI { get; set; }
+        public int PercentualBorracha { get; set; }
+        public bool Estourado { get; set; }
+        public int VelocidadeMaxima { get; set; }
+        public int VelocidadeAtual { get; set; }
+        public bool Estepe { get; set; }
+
+        public Pneu(int  _aro, int _velocidadeMaxima, string _tipo, bool _estepe)
+        {
+            Aro = _aro;
+            tipo = _tipo;
+            VelocidadeMaxima = _velocidadeMaxima;
+            Estepe = _estepe;
+
+            VelocidadeAtual = 0;
+            Estourado = false;
+            TWI = false;
+            Cor = "Preto";
+            PercentualBorracha = 100;
+        }
+
+
+        public void Girar(int _velocidade)
+        {
+            VelocidadeAtual = VelocidadeAtual + _velocidade;
+            PercentualBorracha = PercentualBorracha - 3;
+                         
+            if (VelocidadeAtual > VelocidadeMaxima || PercentualBorracha == 30)
+            {
+                EstourarPneu();              
+            }
+        }
+
+        public void EstourarPneu()
+        {
+            Estourado = true;
+            VelocidadeAtual = 0;
+        }
+
+        public void Frear(int _reducao)
+        {
+            VelocidadeAtual = VelocidadeAtual - _reducao;
+            PercentualBorracha = PercentualBorracha - 5;
+
+            if (PercentualBorracha <= 30)
+            {
+                EstourarPneu();
+            }
+
+            if (VelocidadeAtual < 0)
+            {
+                Estourado = true;
+                VelocidadeAtual = 0;
+            }
+        }
+
+            public void Exibir()
+            {
+                Console.WriteLine("Aro: " + Aro);
+                Console.WriteLine("PercentualBorracha: " + PercentualBorracha);
+                Console.WriteLine("Cor: " + Cor);
+                Console.WriteLine("VelocidadeMaxima: " + VelocidadeMaxima);
+                Console.WriteLine("Estepe: " + Estepe);
+                Console.WriteLine("Estourado: " + Estourado);
+                Console.WriteLine("tipo: " + tipo);
+                Console.WriteLine("TWI: " + TWI);
+                Console.WriteLine("VelocidadeAtual: " + VelocidadeAtual);
+            }
+        }
+    }
